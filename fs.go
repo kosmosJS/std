a507eee0ab18c79f37eaf1e1a391611aefd62723
satyrnwastaken <satyrn@disroot.org>
2022-04-24 18:31:36 +0000

    WIP

---
+++ fs.go
@@ -0,0 +1,17 @@
+package fs
+
+import (
+	"github.com/kosmosJS/engine-node/require"
+	"github.com/kosmosJS/engine"
+	"os"
+)
+
+func RegisterFS() {
+	require.RegisterNativeModule("fs", func(runtime *engine.Runtime, module *engine.Object) {
+		o := module.Get("exports").(*engine.Object)
+
+		o.Set("readFile", func(call engine.FunctionCall, name string) engine.Value {
+			return runtime.ToValue(name)
+		})
+	})
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/kosmosJS/std
+
+go 1.18
+
+require (
+	github.com/dlclark/regexp2 v1.4.1-0.20201116162257-a2a8dda75c91 // indirect
+	github.com/go-sourcemap/sourcemap v2.1.3+incompatible // indirect
+	github.com/kosmosJS/engine v0.0.0-20220424180524-241d93cbe6db // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
